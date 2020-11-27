7912d379afcda889476b17b932fe48efc5b08990
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-27 10:17:58 +0300

    initial

---
+++ src/test/java/ru/netology/card/CardTest.java
@@ -0,0 +1,30 @@
+package ru.netology.card;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardTest {
+
+    @BeforeEach
+    void setUp(){
+        open("http://localhost:9999");
+    }
+    @Test
+    void shouldSubmitRequest(){
+        SelenideElement form = $("[data-test-id=card-form]");
+        
+        form.$("[data-test-id=name] input").setValue("Петров Иван");
+        form.$("[data-test-id=phone] input").setValue("+79999999999");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $("[data-test -id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена"));
+
+
+    }
+}
